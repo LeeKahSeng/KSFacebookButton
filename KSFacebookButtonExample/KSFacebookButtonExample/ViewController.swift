d43d170a8aafc3da3537de8e0d46ac2162d416bd
Lee Kah Seng <kahseng.lee123@gmail.com>
2017-05-17 15:32:08 +0800

    Created project

---
+++ KSFacebookButtonExample/KSFacebookButtonExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KSFacebookButtonExample
+//
+//  Created by Lee Kah Seng on 17/05/2017.
+//  Copyright © 2017 Lee Kah Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sources/KSFacebookButton.swift
@@ -0,0 +1,9 @@
+//
+//  KSFacebookButton.swift
+//  KSFacebookButtonExample
+//
+//  Created by Lee Kah Seng on 17/05/2017.
+//  Copyright © 2017 Lee Kah Seng. All rights reserved.
+//
+
+import Foundation
